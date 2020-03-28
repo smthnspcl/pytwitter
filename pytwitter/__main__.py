8ed1bb79a4229231dad999795ffbacbb1d2aa068
smthnspcl <pascal@eberlein.io>
2020-03-28 17:41:33 +0100

    update

--- pytwitter/__main__.py
+++ pytwitter/__main__.py
@@ -1,55 +1,15 @@
-from os import pathsep
-from sys import argv
+from argparse import ArgumentParser
 from . import Twitter
 
 
-class Configuration(object):
-    users = []
-    output = "out" + pathsep
-    count = 1000
-
-    @staticmethod
-    def help():
-        print("usage:")
-        print("\tunix: ./twitter.py {arguments}")
-        print("\twin:  .\\twitter.exe {arguments}")
-        print("{arguments}\t\t\t{description}\t\t{default}")
-        print("\t-u\t--username\tadd a username\t\tNone")
-        print("\t-o\t--output\tset output dir\t\tout")
-        print("\t-c\t--count\t\tcount of tweets\t\t1000")
-        print("{example}")
-        print(".\\twitter.exe -u s8n -u theydeadinside -o dankmemes")
-        print("    \\-> executable|       \\-> 2nd username    \\-> output folder")
-        print("                  \\-> 1st username")
-        exit()
-
-    @staticmethod
-    def parse():
-        c = Configuration()
-        i = 0
-        while i < len(argv):
-            a = argv[i]
-            if a in ["-u", "--username"]:
-                c.users.append(argv[i + 1])
-            elif a in ["-o", "--output"]:
-                c.output = argv[i + 1]
-            elif a in ["-c", "--count"]:
-                c.count = int(argv[i + 1])
-            elif a in ["-h", "--help"]:
-                Configuration.help()
-            i += 1
-        if not c.output.endswith(pathsep):
-            c.output += pathsep
-        if len(c.users) == 0:
-            Configuration.help()
-
-        return c
-
-
 if __name__ == '__main__':
-    args = Configuration.parse()
-    twitter = Twitter()
-    for user in args.users:
+    ap = ArgumentParser()
+    ap.add_argument("-u", "--username", "--usernames", action="append", nargs='+', required=True)
+    ap.add_argument("-d", "--database", default="sqlite:///twitter.db", type=str)
+    ap.add_argument("-c", "--count", default=100, type=int)
+    a = ap.parse_args()
+    twitter = Twitter(database=a.database)
+    for user in a.usernames:
         u = twitter.get_user(user)
-        u.get_steam_items(100)
+        u.get_steam_items(a.count)
     twitter.close()
